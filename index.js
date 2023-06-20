5d92524822a32a0ade4bd904d26e3d1fcc8ff3da
James <85806019+James-Discord@users.noreply.github.com>
2023-06-20 17:55:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,73 @@
-const express = require('express');
-const { Configuration, OpenAIApi } = require('openai');
+const Discord = require('discord.js');
 const axios = require('axios');
-const multer = require('multer');
-const path = require('path');
-const FormData = require('form-data');
 const fs = require('fs');
+const FormData = require('form-data');
 
-const app = express();
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: 'uploads/',
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    },
-  }),
+const client = new Discord.Client();
+const token = 'YOUR_DISCORD_BOT_TOKEN';
+const transcriptionsAPI = 'https://api.cattto.repl.co/v1/audio/transcriptions';
+const apiKey = 'YOUR_API_KEY';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
-app.use(express.json());
+client.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (message.content === '!start') {
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      message.reply('You need to join a voice channel first.');
+      return;
+    }
 
-app.post('/transcriptions', upload.single('file'), async (req, res) => {
-  const { originalname, path: filePath } = req.file;
-  const model = req.body.model || 'whisper-1';
-  const token = 'catto_key_UVSctZHJmQo2IQh0nnfiZUBW';
+    const connection = await voiceChannel.join();
+    const receiver = connection.receiver;
 
-  try {
-    const formData = new FormData();
-    formData.append('file', fs.createReadStream(filePath));
-    formData.append('model', model);
-
-    console.time('Transcription Time');
-    const response = await axios.post(
-      'https://api.cattto.repl.co/v1/audio/transcriptions',
-      formData,
-      {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
-        },
+    connection.on('speaking', async (user, speaking) => {
+      if (speaking.bitfield === 0 || user.bot) {
+        return;
       }
-    );
-    console.timeEnd('Transcription Time');
+      console.log(`Listening to ${user.username}`);
 
-    res.json(response.data);
-  } catch (error) {
-    console.error('Transcription failed:', error.message);
-    res.status(500).json({ error: 'Transcription failed.' });
-  }
-});
+      const audioStream = receiver.createStream(user, { mode: 'pcm' });
 
-app.get('/gpt/gpt-3.5turbo', async (req, res) => {
-  const { prompt } = req.query;
-  const promptMessages = [
-    { role: 'system', content: 'You are a helpful assistant.' },
-    { role: 'user', content: prompt },
-  ];
-  
-  const apiKey = 'catto_key_UVSctZHJmQo2IQh0nnfiZUBW';
-  const apiUrl = 'https://api.cattto.repl.co/v1/chat/completions';
-  
-  try {
-    const response = await axios.post(apiUrl, {
-      model: 'gpt-3.5-turbo',
-      messages: promptMessages,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-      },
+      const audioPath = `./${user.id}.pcm`;
+      audioStream.pipe(fs.createWriteStream(audioPath));
+
+      audioStream.on('end', async () => {
+        try {
+          const formData = new FormData();
+          formData.append('file', fs.createReadStream(audioPath));
+          formData.append('model', 'whisper-1');
+
+          const transcriptionResponse = await axios.post(transcriptionsAPI, formData, {
+            headers: {
+              'Authorization': `Bearer ${apiKey}`,
+              'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+            },
+          });
+
+          const transcription = transcriptionResponse.data.transcription;
+          message.channel.send(`Transcription (${user.username}): ${transcription}`);
+        } catch (error) {
+          console.error('Transcription failed:', error.message);
+          message.channel.send('Transcription failed.');
+        } finally {
+          fs.unlinkSync(audioPath);
+        }
+      });
     });
+  } else if (message.content === '!stop') {
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      message.reply('You need to join a voice channel first.');
+      return;
+    }
 
-    const generatedResponse = response.data.choices[0].message.content;
-    res.json({ response: generatedResponse });
-  } catch (error) {
-    console.error('OpenAI API request failed:', error.message);
-    res.status(500).json({ error: 'OpenAI API request failed.' });
+    voiceChannel.leave();
+    message.reply('Stopped transcribing.');
   }
 });
 
-
-app.listen(3000, () => {
-  console.log('API server is running on port 3000');
-});
+client.login(token);
